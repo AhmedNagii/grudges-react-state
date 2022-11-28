5164ef7bba4788415ab5e480e74d5b7adb4d544c
Ahmed nagi <36850727+AhmedNagii@users.noreply.github.com>
2022-11-28 13:44:21 +0200

    implement reducer

---
+++ src/GrudgeContext.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { useReducer, useCallback, createContext } from "react";
+
+
+import id from 'uuid/v4';
+import initialState from './initialState';
+
+
+const GrudgeContext = createContext();
+
+const GRUDGE_ADD = "GRUDGE_ADD";
+const GRUDGE_FORGIVE = "GRUDGE_FORGIVE";
+
+
+const reducer = (state, action) => {
+    if (action.type === GRUDGE_ADD) {
+        return [
+            action.payload,
+            ...state
+        ];
+    }
+    if (action.type === GRUDGE_FORGIVE) {
+
+        return state.map(grudge => {
+            if (grudge.id !== action.payload.id) return grudge;
+            return { ...grudge, forgiven: !grudge.forgiven };
+        });
+    }
+    return state;
+};
+
+export const GrudgeProvider = ({ children }) => {
+    const [grudges, dispatch] = useReducer(reducer, initialState);
+
+    const addGrudge = useCallback(({ person, reason }) => {
+        dispatch({
+            type: GRUDGE_ADD,
+            payload: {
+                person,
+                reason,
+                forgiven: false,
+                id: id()
+            }
+        });
+    }, [dispatch]);
+
+    const toggleForgiveness = useCallback(id => {
+        dispatch({
+            type: GRUDGE_FORGIVE,
+            payload: { id }
+        });
+    }, [dispatch]);
+
+    return (
+        <GrudgeContext.Provider value={{ grudges, toggleForgiveness, addGrudge }}>
+            {children}
+        </GrudgeContext.Provider>
+    );
+
+};
+
+export { GrudgeContext };
--- src/NewGrudge.js
+++ src/NewGrudge.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { GrudgeContext } from './GrudgeContext';
 
-const NewGrudge = ({ onSubmit }) => {
+
+const NewGrudge = React.memo(() => {
   const [person, setPerson] = useState('');
   const [reason, setReason] = useState('');
+  const { addGrudge } = useContext(GrudgeContext);
 
   const handleChange = event => {
     event.preventDefault();
-    onSubmit({ person, reason });
+    addGrudge({ person, reason });
   };
 
   return (
@@ -28,6 +31,6 @@ const NewGrudge = ({ onSubmit }) => {
       <input className="NewGrudge-submit button" type="submit" />
     </form>
   );
-};
+});
 
 export default NewGrudge;
